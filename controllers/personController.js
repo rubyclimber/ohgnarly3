879740240df6005f6e173348b941df74bbb4ee66
Aaron <asmitty92@gmail.com>
2019-09-18 08:32:27 -0500

    Update person controller to receive correct file-ripper output json

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,11 +1,14 @@
 const Person = require('../models/person')
 
 module.exports.createPerson = (req, res) => {
-    var person = new Person({
-        name: req.body.name,
-        age: req.body.age,
-        dob: req.body.dob
-    });
-    person.save()
-    res.send(person)
+    var people = req.body.records;
+    for (var item in people) {
+        var person = new Person({
+            name: item.name,
+            age: item.age,
+            dob: item.dob
+        });
+        person.save();
+    }
+    res.send({success: true});
 };
