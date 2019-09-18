f2ebdd7cb8b283e72ba908a1a80c3abac9c751d0
Aaron <asmitty92@gmail.com>
2019-09-18 08:49:57 -0500

    Bug fixes

--- controllers/personController.js
+++ controllers/personController.js
@@ -2,13 +2,15 @@ const Person = require('../models/person')
 
 module.exports.createPerson = (req, res) => {
     var people = req.body.records;
-    for (var item in people) {
+    var ids = [];
+    for (var item of people) {
         var person = new Person({
             name: item.name,
             age: item.age,
             dob: item.dob
         });
         person.save();
+        ids.push(person._id)
     }
-    res.send({success: true});
+    res.send({success: true, ids: ids});
 };
